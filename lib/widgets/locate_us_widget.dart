96fc4869afda3916470f63c6f7c83ede3238a582
Lans12345 <olanalans12345@gmail.com>
2024-04-27 07:54:54 +0800

    added initial map

--- lib/widgets/locate_us_widget.dart
+++ lib/widgets/locate_us_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:mcakes/widgets/text_widget.dart';
+import 'package:latlong2/latlong.dart';
 
 class LocateUsWidget extends StatelessWidget {
   const LocateUsWidget({super.key});
@@ -27,6 +29,18 @@ class LocateUsWidget extends StatelessWidget {
           height: 500,
           decoration: BoxDecoration(
             color: Colors.grey[100],
+          ),
+          child: FlutterMap(
+            options: const MapOptions(
+              initialCenter: LatLng(8.3451, 124.9767),
+              initialZoom: 9.2,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ],
           ),
         ),
       ],
