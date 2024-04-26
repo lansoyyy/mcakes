b4a42541d1ec5064178539ee31086f88ccf7a723
Lans12345 <olanalans12345@gmail.com>
2024-04-27 07:58:04 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mcakes/screens/auth/business_login_screen.dart';
 import 'package:mcakes/screens/auth/login_screen.dart';
 import 'package:mcakes/screens/business/business_home_screen.dart';
 import 'package:mcakes/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCCv99Fb2psouQR5omHd2cHFs39Tuy7kkA",
+          authDomain: "mcakes-7e3b1.firebaseapp.com",
+          projectId: "mcakes-7e3b1",
+          storageBucket: "mcakes-7e3b1.appspot.com",
+          messagingSenderId: "334546460507",
+          appId: "1:334546460507:web:62df3f84c8d4b0f1aa447e"));
   runApp(const MyApp());
 }
 
@@ -16,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'MCakes',
-      home: HomeScreen(),
+      home: LoginPage(),
     );
   }
 }
